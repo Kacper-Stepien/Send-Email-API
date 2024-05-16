c21c96e29fc935c4d6cc502d6ed6106f716f5117
Kacper Stępień <kacper2007x48@gmail.com>
2024-05-16 15:39:35 +0200

    set CROSS ORIGIN POLICY

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ const port = 3000;
 dotenv.config({ path: "./config.env" });
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const transporter = nodeMailer.createTransport({
   service: process.env.SERVICE,
   host: process.env.HOST,
