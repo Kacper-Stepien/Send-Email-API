a202701804fcf1df01b5a4748a67a2f8f0d7037b
Kacper Stępień <kacper2007x48@gmail.com>
2024-05-16 14:37:54 +0200

    create send email api

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const nodeMailer = require("nodemailer");
+const app = express();
+const port = 3000;
+
+dotenv.config({ path: "./config.env" });
+app.use(express.json());
+
+const transporter = nodeMailer.createTransport({
+  service: process.env.SERVICE,
+  host: process.env.HOST,
+  port: process.env.PORT,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.post("/send-email", (req, res) => {
+  const { name, surname, message, recipient, sender } = req.body;
+  if (!name || !surname || !message || !recipient || !sender) {
+    return res.status(400).send("All fields are required");
+  }
+
+  const mailOptions = {
+    from: {
+      name: sender,
+      address: process.env.EMAIL_USER,
+    },
+    to: recipient,
+    subject: `Wiadomość od ${name} ${surname}`,
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(500).send("Error sending email");
+    } else {
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
